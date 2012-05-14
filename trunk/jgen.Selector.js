e5814cf04729ac0ba445441bdb872a65846b45ac
matveev.ruslan <matveev.ruslan@767c675e-8d8d-ae7e-3894-c4cacb5b5dd5>
2012-05-14 20:13:47 +0000

    sync

--- trunk/jgen.Selector.js
+++ trunk/jgen.Selector.js
@@ -25,25 +25,25 @@
 jsface.namespace("jgen");
 
 jsface.def({
-	
+
 	$meta: {
 		name: "Selector",
 		namespace: jgen,
 		singleton: true
 	},
-	
+
 	query: function(oElement, sSelector) {
 		return oElement.querySelector(sSelector);
 	},
-	
+
 	queryAll: function(oElement, sSelector) {
 		return oElement.querySelectorAll(sSelector);
 	},
-	
+
 	matches: function(oElement, sSelector) {
 		return oElement.webkitMatchesSelector(sSelector);
 	},
-	
+
 	queryAncestor: function(oElement, sSelector) {
 		if (this.matches(oElement, sSelector)) return oElement;
 		if ((oElement.parentNode) && (oElement.parentNode.nodeType == 1)) {
@@ -51,5 +51,5 @@ jsface.def({
 		}
 		return null;
 	}
-	
+
 });
